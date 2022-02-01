4023f50a3e0a47558c84e1cfb2705eb4150e2742
Joao Estudante <estudante.jm@gmail.com>
2022-02-01 18:34:50 +0000

    second idea strlcat

--- 42/projects/c_03/ex05/ft_strlcat.c
+++ 42/projects/c_03/ex05/ft_strlcat.c
@@ -1,18 +1,34 @@
 
 
 
-#include <unistd.h>
+#include <stdio.h>
 
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	int	*tmpdest;
-	int	i;
-
+	unsigned int	i;
+	unsigned int	j;
+	
 	i = 0;
-	tmpdest = src;
-	while (*src++)
-		while (i < size - 1)
-			if (!(dest[i++]))
-				dest[i - 1] = *(src - 1);
+	j = -1;
+	while (dest[i])//Get len of dest.
+		i++;
+	while (src[++j])//count src len
+		if (size - 1 - (i + j) > 0)//if theres space
+			*(dest++ + i) = src[j];//assign the value in src to dest
+	*dest = '\0';//terminating the result
+	if (size < i)
+		return (size + j);
+	return (i + j);
+}
 
+int	main(void)
+{
+	char	src[] = "cake";
+	char	dest[] = "i love ";
+	int	result;
+
+	result =  ft_strlcat(dest, src, 12);
+	printf("%s\n", dest);
+	printf("%d\n", result);
+	return (0);
 }
